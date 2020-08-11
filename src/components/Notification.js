64f1e742700dda9ace938adff65a12b920c078fb
Maria Shimkovska <mimi@Marias-MBP.lan>
2020-08-11 15:44:55 -0700

    added more changes to start making the js files into classes

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -4,16 +4,20 @@ import React from 'react';
  * Notifications component, 
  * pass showNotification prop
  */
-const Notification = ({showNotification}) => {
+export default class Notification extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+  render() {
   return (
       // conditional className, notification shows when prop returns true
       /** There are ways to improve this code. Right now the notification is hidden by not being in the window but below it. 
        * I could figure out a way to just not render it if showNotification is false.
        */
-    <div className={`notification-container ${showNotification ? 'show' : ''}`}>
+    <div className={`notification-container ${this.props.showNotification ? 'show' : ''}`}>
         <p>You have already entered this letter before.</p>
     </div>
   );
+  }
 }
-
-export default Notification;
--- src/components/Popup.js
+++ src/components/Popup.js
@@ -8,64 +8,45 @@ class Popup extends React.Component {
      */
     constructor(props) {
         super(props);
-        this.state = {
-            playable: true,
-            finalMessage: '',
-            finalMessageRevealWord: ''
-        }
-
+        this.playable = true;
+        this.finalMessage = '';
+        this.finalMessageRevealWord = '';
         /**
          * For methods in react, the this keyword should represent the component that
          * owns the method. 
          * That is why you should use arrow functions
          */
-       // this.playAgain = this.playAgain.bind(this)
+        // this.playAgain = this.playAgain.bind(this)
     }
 
     componentDidUpdate() {
         if (checkWin(this.props.correctLetters, this.props.wrongLetters, this.props.selectedWord) === 'win') {
             console.log("winning")
-            // this.setState((prevState) => {
-            //     return {
-            //        ...prevState.playable = false,
-            //        ...prevState.finalMessage= 'Congratulations! You won! 😃',
-            //        ...prevState.finalMessageRevealWord
-            //     }
-            //    }
-            //    );
+            this.playable = false;
+            this.finalMessage = 'Congratulations! You won! 😃';
+            this.finalMessageRevealWord = '';
         } else if (checkWin(this.props.correctLetters, this.props.wrongLetters, this.props.selectedWord) === 'lose') {
             console.log("losing")
-            // this.setState(() => {
-            //  return {
-            //     playable: false,
-            //     finalMessage: 'Unfortunately you lost. 😕',
-            //     finalMessageRevealWord:  `...the word was: ${this.props.selectedWord}`
-            //  }
-            // }
-            // );
+            this.playable = false;
+            this.finalMessage = 'Unfortunately you lost. 😕';
+            this.finalMessageRevealWord = `...the word was: ${this.props.selectedWord}`;
             console.log(this.props);
-
         }
     }
 
     playAgain = () => {
-        this.setPlayable = this.props.playable;
-        
-        this.setState(() => {
-            return {
-                playable: true,
-                finalMessage: 'Unfortunately you lost. 😕',
-                finalMessageRevealWord: `...the word was: ${this.props.selectedWord}`
-            }
-        }
-        );
+        console.log("clicking")
+        // this.setPlayable = this.playable;
+        this.playable = true;
+        this.finalMessage = '';
+        this.finalMessageRevealWord = '';
     }
 
 
 
     render() {
         return (
-            <div className="popup-container" style={this.state.finalMessage !== '' ? { display: 'flex' } : {}}>
+            <div className="popup-container" style={this.finalMessage !== '' ? { display: 'flex' } : {}}>
                 <div className="popup">
                     <h2>{this.finalMessage}</h2>
                     <h3>{this.finalMessageRevealWord}</h3>
